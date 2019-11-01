9e6613e9565653af7daa5d69cfba1e785d86b33e
nixsoares13@homail.com <nickgithub301298>
2019-11-01 10:17:58 -0300

    Atualized

--- Animal.java
+++ Animal.java
@@ -5,7 +5,7 @@ public class Animal {
     //defaults methods
     private String name;
     private int weight;
-    private  boolean hasOwner = false;
+    private boolean hasOwner = false;
     private char favorateChar;
     private long uniqueID;  //this goes from 2^32 till 2^-32
     private double speed;
@@ -13,11 +13,69 @@ public class Animal {
 
 
     protected static int numberOfAnimals = 0;
-    static Scanner userinput = new Scanner(System.in);
+    //protected means that this value can be accessed only by other code in the package
+    //static shares the same value for every single object
+    static Scanner userInput = new Scanner(System.in); //data from the keyboard
+//Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public boolean isHasOwner() {
+        return hasOwner;
+    }
+
+    public char getFavorateChar() {
+        return favorateChar;
+    }
+
+    public long getUniqueID() {
+        return uniqueID;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+//Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setHasOwner(boolean hasOwner) {
+        this.hasOwner = hasOwner;
+    }
+
+    public void setFavorateChar(char favorateChar) {
+        this.favorateChar = favorateChar;
+    }
+
+    public void setUniqueID(long uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
 
     //creating the constructor
     public Animal(){
-     numberOfAnimals= numberOfAnimals +1;
+     numberOfAnimals++;
 
      int sumOfNumbers= 5+2;
      System.out.println("5 + 1 =" + sumOfNumbers);
@@ -29,10 +87,17 @@ public class Animal {
         int divOfNumbers= 5/2;
         System.out.println("5 / 1 =" + divOfNumbers);
 
+        System.out.print("Enter the name: \n");
+        if (userInput.hasNextLine()){
+            this.setName(userInput.nextLine());
+
+        }
     }
 
     //main
     public static void main(String[] args) {
-        Animal animal = new Animal();
+        // creating object theAnimal w/ all the propeties of the class Animal()
+        Animal theAnimal = new Animal();
     }
 }
+
